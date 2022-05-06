0513f94ac1ff1b7dcd2322ad5eb1cc6d62821ad7
Hawke <crow7277@gmail.com>
2022-05-06 22:21:13 +0900

    Modal组件开发

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -69,19 +69,59 @@
                     <img src="/imgs/banner-1.png" />
                 </a>
             </div>
-            <div class="product-box">123123</div>
+        </div>
+        <div class="product-box">
+            <div class="container">
+                <h2>手机</h2>
+                <div class="wrapper">
+                    <div class="banner-left">
+                        <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt="" /></a>
+                    </div>
+                    <div class="list-box">
+                        <div class="list" v-for="(arr, index) in phoneList" :key="index">
+                            <div class="item" v-for="(item, i) in arr" :key="i">
+                                <span :class="{ 'new-pro': i % 2 === 0 }">新品</span>
+                                <div class="item-img">
+                                    <img :src="item.mainImage" alt="" />
+                                </div>
+                                <div class="item-info">
+                                    <h3>{{ item.name }}</h3>
+                                    <p>{{ item.subtitle }}</p>
+                                    <p class="price" @click="addCart(item.id)">
+                                        {{ item.price }}元
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <ServiceBar></ServiceBar>
+        <Modal
+            title="提示"
+            sureText="查看购物车"
+            btnType="1"
+            modalType="middle"
+            :showModal="showModal"
+            @submit="gotoCart"
+            @cancel="showModal = false"
+        >
+            <template v-slot:body>
+                <p>商品添加成功！</p>
+            </template>
+        </Modal>
     </div>
 </template>
 
 <script>
 import ServiceBar from '@/components/ServiceBar.vue';
+import Modal from '@/components/Modal.vue';
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
 import 'swiper/css/swiper.css';
 export default {
     name: 'Index',
-    components: { ServiceBar, Swiper, SwiperSlide },
+    components: { ServiceBar, Swiper, SwiperSlide, Modal },
     data() {
         return {
             swiperOptions: {
@@ -170,7 +210,46 @@ export default {
                     img: require('/public/imgs/ads/ads-4.jpg'),
                 },
             ],
+            phoneList: [
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+            ],
+            showModal: false,
         };
+    },
+    mounted() {
+        this.init();
+    },
+    methods: {
+        init() {
+            this.axios
+                .get('/products', {
+                    params: {
+                        categoryId: 100012,
+                        pageSize: 14,
+                    },
+                })
+                .then(res => {
+                    res.list = res.list.slice(6, 14);
+                    // 利用slice分割生成二维数组
+                    this.phoneList = [res.list.slice(0, 4), res.list.slice(4, 8)];
+                });
+        },
+        addCart(id) {
+            this.showModal = true;
+            return;
+            /* this.axios
+                .post('/carts', {
+                    productId: id,
+                    selected: true,
+                })
+                .then()
+                .catch(() => {
+                });*/
+        },
+        gotoCart() {
+            this.$router.push('/cart');
+        },
     },
 };
 </script>
@@ -269,6 +348,86 @@ export default {
     }
     .banner {
         margin-bottom: 50px;
+    }
+    .product-box {
+        background-color: $colorJ;
+        padding: 30px 0 50px;
+        h2 {
+            font-size: $fontF;
+            height: 21px;
+            line-height: 21px;
+            color: $colorB;
+            margin-bottom: 20px;
+        }
+        .wrapper {
+            display: flex;
+            .banner-left {
+                margin-left: 16px;
+                img {
+                    width: 224px;
+                    height: 619px;
+                }
+            }
+            .list {
+                @include flex();
+                width: 986px;
+                margin-bottom: 14px;
+                &:last-child {
+                    margin-bottom: 0;
+                }
+                .item {
+                    width: 236px;
+                    height: 302px;
+                    background-color: $colorG;
+                    text-align: center;
+                    span {
+                        display: inline-block;
+                        width: 67px;
+                        height: 24px;
+                        font-size: 14px;
+                        line-height: 24px;
+                        color: $colorG;
+                        &.new-pro {
+                            background-color: #7ecf68;
+                        }
+                        &.kill-pro {
+                            background-color: #e82626;
+                        }
+                    }
+                    .item-img {
+                        img {
+                            height: 195px;
+                            width: 100%;
+                        }
+                    }
+                    .item-info {
+                        h3 {
+                            font-size: $fontJ;
+                            color: $colorB;
+                            line-height: $fontJ;
+                            font-weight: bold;
+                        }
+                        p {
+                            color: $colorD;
+                            line-height: 13px;
+                            margin: 6px auto 13px;
+                        }
+                        .price {
+                            color: #f20a0a;
+                            font-size: $fontJ;
+                            font-weight: bold;
+                            cursor: pointer;
+                            &:after {
+                                @include bgImg(22px, 22px, '/public/imgs/icon-cart-hover.png');
+                                content: ' ';
+                                margin-left: 5px;
+                                vertical-align: middle;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 </style>
