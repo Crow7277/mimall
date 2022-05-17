ab307e31264254c69123396360a26868ab9b78c3
Hawke <crow7277@gmail.com>
2022-05-17 22:31:59 +0900

    路由懒加载——利用import

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,6 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/pages/home';
 import Index from '@/pages/index';
-import Product from '@/pages/product';
-import Detail from '@/pages/detail';
-import Cart from '@/pages/cart';
-import Order from '@/pages/order';
-import OrderConfirm from '@/pages/orderConfirm';
-import OrderList from '@/pages/orderList';
-import OrderPay from '@/pages/orderPay';
-import Login from '@/pages/login';
-import Alipay from '@/pages/alipay';
 
 Vue.use(VueRouter);
 
@@ -29,54 +20,49 @@ const routes = [
             {
                 path: '/product/:id', //动态路由
                 name: 'product',
-                component: Product,
+                component: () => import('@/pages/product'),
             },
             {
                 path: '/detail/:id',
                 name: 'detail',
-                component: Detail,
-            },
-            {
-                path: '/detail/:id',
-                name: 'detail',
-                component: Detail,
+                component: () => import('@/pages/detail'),
             },
         ],
     },
     {
         path: '/login',
         name: 'login',
-        component: Login,
+        component: () => import('@/pages/login'),
     },
     {
         path: '/cart',
         name: 'cart',
-        component: Cart,
+        component: () => import('@/pages/cart'),
     },
     {
         path: '/order',
         name: 'order',
-        component: Order,
+        component: () => import('@/pages/order'),
         children: [
             {
                 path: 'confirm',
                 name: 'order-confirm',
-                component: OrderConfirm,
+                component: () => import('@/pages/orderConfirm'),
             },
             {
                 path: 'list',
                 name: 'order-list',
-                component: OrderList,
+                component: () => import('@/pages/orderList'),
             },
             {
                 path: 'pay',
                 name: 'order-pay',
-                component: OrderPay,
+                component: () => import('@/pages/orderPay'),
             },
             {
                 path: 'alipay',
                 name: 'alipay',
-                component: Alipay,
+                component: () => import('@/pages/alipay'),
             },
         ],
     },
